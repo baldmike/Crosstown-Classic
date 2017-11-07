b1e48b20e583dcdbc03519af01202ea48cf24e03
Bald Mike <baldmike312@gmail.com>
2017-11-06 23:43:27 -0600

    Player model & both teams added

--- apps/baseball_app/models.py
+++ apps/baseball_app/models.py
@@ -3,15 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
  
 class Player(models.Model):
+    team = models.CharField(max_length = 30)
     order = models.IntegerField()
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     position = models.CharField(max_length = 30)
-    strike_num = models.IntegerField()
-    out_num = models.IntegerField()
-    single_num = models.IntegerField()
-    double_num = models.IntegerField()
-    triple_num = models.IntegerField()
+    strike_num = models.IntegerField(default = 20)
+    hr_num = models.IntegerField(default = 25)
+    single_num = models.IntegerField(default = 35)
+    double_num = models.IntegerField(default = 40)
+    triple_num = models.IntegerField(default = 44)
+    hbp_num = models.IntegerField(default = 45)
     
     
 
