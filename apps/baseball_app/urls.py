f1a40407087cad5d8ffa2c03cd6150d6eb7dd80d
Bald Mike <baldmike312@gmail.com>
2017-11-12 15:37:49 -0600

    Sunday commit

--- apps/baseball_app/urls.py
+++ apps/baseball_app/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from views import index, watch, swing, reset
+from views import index, watch, swing, reset, game_over
 
 urlpatterns = [
     url(r'^$', index),
     url(r'^watch$', watch),
     url(r'^swing$', swing),
-    url(r'^reset$', reset)
+    url(r'^reset$', reset),
+    url(r'^game_over$', game_over)
 ]
